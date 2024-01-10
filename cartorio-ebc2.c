0978f7179473ef69b8cd310a04dfe0d3f9ba7b23
ReisxJeff <156038740+ReisxJeff@users.noreply.github.com>
2024-01-09 22:50:36 -0300

    opção de saída

--- cartorio-ebc2.c
+++ cartorio-ebc2.c
@@ -138,7 +138,8 @@ int main()
         printf("escolha a opção desejada no menu:\n\n ");
         printf("\t1 - Resgistrar nomes\n");
         printf("\t2 - Consultar nomes\n");
-        printf("\t3 - deletar nomes\n\n");
+        printf("\t3 - deletar nomes\n");
+        printf("\t4 - sair do sistema\n\n");
 	    printf( "Opção: ");// fim do menu 
     
         scanf("%d", &opcao);// armazenando a escolha do usuario 
@@ -156,6 +157,10 @@ int main()
 			case 3:
 			    deletar();
 			    break;
+			case 4:
+				printf("obrigado por utilizar o sistema");
+				return 0;
+				break;
 			default:
 				printf("\tEssa opção não está disponivel !\n");
 				system ("pause");
